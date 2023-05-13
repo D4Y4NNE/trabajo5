e769216b318a0df925681855b907bc831cf9de01
DAYANNE MAYTA DE LA CRUZ <103327196+D4Y4NNE@users.noreply.github.com>
2023-05-12 19:55:14 -0500

    Add files via upload

---
+++ ECUACION_CUADRATICA/out/production/ECUACION_CUADRATICA/Main.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        EcuacionCuadratica.setA(-2);
+        System.out.println(EcuacionCuadratica.getA());
+
+        EcuacionCuadratica.setB(4);
+        System.out.println(EcuacionCuadratica.getB());
+
+        EcuacionCuadratica.setC(6);
+        System.out.println(EcuacionCuadratica.getC());
+
+        EcuacionCuadratica.raices();
+    }
+}
